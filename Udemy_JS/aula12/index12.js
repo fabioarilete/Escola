14a5f966e21eb809c725d0572b538ed8c78e730e
fabioarilete <fabioarilete@hotmail.com>
2023-08-04 21:17:48 -0300

    att

---
+++ Udemy_JS/aula12/index12.js
@@ -0,0 +1,17 @@
+// const num1 = prompt('Digite um número:')
+// const num2 = prompt('Digite outro número:')
+
+// const num01 = Number(num1)
+// const num02 = Number(num2)
+
+// const resultado = num01 + num02
+
+// alert(`O resultado da soma de ${num1} mais ${num2} é de ${resultado}.`)
+
+let num1 = prompt("Digite um numero");
+let num2 = prompt("Digite outro numero");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+alert(`O resultado da soma de ${num1} e ${num2} é de ${num1 + num2}`);
