5940b5dbbc876a005461582b2b08dd27c65a7aef
Fábio Henrique <fabioarilete@hotmail.com>
2023-08-07 07:31:09 -0300

    att

---
+++ javascript/Aulas diversas/Aula10/exercicio_Condic_Simples.js
@@ -0,0 +1,8 @@
+var vel = 40.5;
+console.log(`Avelocidade do seu carro é ${vel}km/h`);
+if (vel > 50) {
+  //Condição Simples
+  console.log(`Você ultrapassou o limite de velocidade! Será MULTADO!`);
+}
+console.log(`Dirija sempre usando cinto de segurança!`);
+console.
---
+++ javascript/Aulas diversas/aula14/aula14.js
@@ -0,0 +1,5 @@
+var tab = 7;
+for (var c = 1; c <= 10; c++) {
+  var res = c * tab;
+  console.log(`${tab} x ${c} = ${res}`);
+}
