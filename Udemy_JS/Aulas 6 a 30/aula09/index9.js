f7bea44fb04291c5d4f63a4a928345a888e23927
fabioarilete <136510190+fabioarilete@users.noreply.github.com>
2023-09-09 12:41:48 -0300

    Delete Udemy_JS directory

--- Udemy_JS/Aulas 6 a 30/aula09/index9.js
+++
@@ -1,9 +0,0 @@
-// diferenças entre var e let
-// ECMAScript 2015 (ES6) JavaScript
-
-nome = 'Luiz' // NÃO FAÇA ISSO (CRIAR VARIÁVEL SEM O 'VAR', 'LET' E 'CONST')
-
-var nome = 'Luiz'
-var nome = 'Otávio'
-
-console.log(nome)
--- Udemy_JS/Aulas 6 a 30/aula25/index25.js
+++
@@ -1,7 +0,0 @@
-const numero = 6;
-
-if (numero >= 0 && numero <= 5) {
-  console.log("O numero está entre 0 e 5.");
-} else if (numero > 5 && numero <= 10) {
-  console.log("O numero está entre 5 e 10.");
-}
